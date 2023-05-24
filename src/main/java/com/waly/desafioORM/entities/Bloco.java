d24c2ace8283b600d84b0a37ad6d5a1dc04db3fa
walyson <walysonmoises1342@gmail.com>
2023-05-24 13:21:49 -0300

    criadas as entidades

---
+++ src/main/java/com/waly/desafioORM/entities/Bloco.java
@@ -0,0 +1,65 @@
+package com.waly.desafioORM.entities;
+
+import jakarta.persistence.*;
+import jdk.jfr.Timespan;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIMEZONE")
+    private Instant inicio;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIMEZONE")
+    private Instant fim;
+
+    public Bloco(Long id, Instant inicio, Instant fim) {
+        this.id = id;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Instant inicio) {
+        this.inicio = inicio;
+    }
+
+    public Instant getFim() {
+        return fim;
+    }
+
+    public void setFim(Instant fim) {
+        this.fim = fim;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Bloco bloco = (Bloco) o;
+
+        return Objects.equals(id, bloco.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
